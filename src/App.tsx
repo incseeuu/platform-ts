1606a3428b3722a0086b3b665fd686b9cd88cfd3
incseeuu <paparazzqq@gmail.com>
2023-05-20 21:04:50 +0300

    helpers, className helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {AboutAsyncPage} from "./page/About/About.async";
 import {HomeAsyncPage} from "./page/Home/Home.async";
 import {Theme} from "./theme/ThemeContext";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames";
 
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
     const {theme,toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {} , [theme])}>
             <button onClick={toggleTheme}>{theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT}</button>
             <Link to={'/'}>Home</Link>
             <Link to={'/about'}>About</Link>
---
+++ src/helpers/classNames.ts
@@ -0,0 +1,14 @@
+
+
+type Mods = Record<string, boolean | string>
+
+export const classNames = (mainClass: string, mods: Mods, additionalClass: string[]): string => {
+
+    return [
+        mainClass,
+        ...additionalClass,
+        ...Object.entries(mods)
+            .filter(([className, isTrue]) => Boolean(isTrue))
+            .map(([className, isTrue]) => className)
+    ].join(' ')
+}
