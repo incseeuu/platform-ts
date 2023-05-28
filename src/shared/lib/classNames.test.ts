f63bb0d09c5f548412f34b7ef0fdb6276ef5b27d
incseeuu <paparazzqq@gmail.com>
2023-05-28 22:00:12 +0300

    add jest, add classNames helper test

---
+++ src/shared/lib/classNames.test.ts
@@ -0,0 +1,17 @@
+import { classNames } from './classNames'
+
+describe('classNames test', () => {
+  test('classNames with mainClass', () => {
+    expect(classNames('mainClass')).toBe('mainClass')
+  })
+
+  test('classNames with main + additionalClass', () => {
+    expect(classNames('mainClass', {}, ['additionalClass']))
+      .toBe('mainClass additionalClass')
+  })
+
+  test('classNames with main + modeClass + additionalClass', () => {
+    expect(classNames('mainClass', { hovered: true }, ['additionalClass']))
+      .toBe('mainClass additionalClass hovered')
+  })
+})
